0e1da3faffb83928a93ddba73d85d03710469a3b
ihnore-ihor <igorbrajchenko@gmail.com>
2025-10-09 23:35:19 +0300

    fix analyzer namespace to analysis

--- Braichenko.Ihor.RobotChallange/Analysis/SituationAnalyzer.cs
+++ Braichenko.Ihor.RobotChallange/Analysis/SituationAnalyzer.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Braichenko.Ihor.RobotChallange.Analyzer
+namespace Braichenko.Ihor.RobotChallange.Analysis
 {
     public class SituationAnalyzer
     {
